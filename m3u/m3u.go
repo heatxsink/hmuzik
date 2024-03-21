739cd7892a15aa03665f4208acdba3d9aafdb4ca
Nicholas Granado <ngranado@gmail.com>
2024-03-21 11:11:05 -0700

    Refactor of 'm3u' module, and new 'playlists' command.

--- m3u/m3u.go
+++ m3u/m3u.go
@@ -1,16 +1,18 @@
 package m3u
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
+
+	"github.com/dhowden/tag"
 )
 
 type Track struct {
 	Path   string
-	Artist string
-	Title  string
+	Info   string
 	Length int
 }
 
@@ -23,21 +25,10 @@ var m3uTemplate = `#EXTM3U
 #EXTENC:UTF-8
 #PLAYLIST:{{ .Title }}
 {{- range .Tracks }}
-#EXTINF:{{ .Length }},{{ .Artist }} - {{ .Title }}
+#EXTINF:{{ .Length }},{{ .Info }}
 {{ .Path }}
 {{- end }}
 `
-
-func NormalizeTitle(cmusPlaylistPath string) string {
-	m3uPath := strings.TrimSuffix(cmusPlaylistPath, filepath.Ext(cmusPlaylistPath))
-	m3uTitle := strings.ReplaceAll(filepath.Base(m3uPath), "-", " ")
-	return strings.ReplaceAll(m3uTitle, "_", " ")
-}
-
-func GleanFilename(cmusPlaylistPath string) string {
-	m3uPath := strings.TrimSuffix(cmusPlaylistPath, filepath.Ext(cmusPlaylistPath))
-	return m3uPath + ".m3u"
-}
 
 func (pl *Playlist) ToFile(path string) error {
 	m3u := template.New("m3u")
@@ -51,4 +42,52 @@ func (pl *Playlist) ToFile(path string) error {
 	}
 	defer f.Close()
 	return m3u.Execute(f, pl)
+}
+
+func NormalizeForTitle(cmusPlaylistPath string) string {
+	m3uTitle := strings.ReplaceAll(filepath.Base(cmusPlaylistPath), "-", " ")
+	return strings.ReplaceAll(m3uTitle, "_", " ")
+}
+
+func Filename(cmusPlaylistPath string, outputPath string) string {
+	m3uBase := fmt.Sprintf("%s.m3u", filepath.Base(cmusPlaylistPath))
+	return filepath.Join(outputPath, m3uBase)
+}
+
+func CreateFromCmusPlaylist(cmusPlaylistPath string, outputPath string, prefix string) error {
+	data, err := os.ReadFile(cmusPlaylistPath)
+	if err != nil {
+		return err
+	}
+	lines := strings.Split(string(data), "\n")
+	tracks := []*Track{}
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		f, err := os.Open(line)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		defer f.Close()
+		track := &Track{
+			Path:   strings.TrimPrefix(line, prefix),
+			Info:   "",
+			Length: -1,
+		}
+		m, err := tag.ReadFrom(f)
+		if err == tag.ErrNoTagsFound {
+			fmt.Println(err, "-->", f.Name())
+			track.Info = strings.TrimSuffix(filepath.Base(line), filepath.Ext(line))
+		} else {
+			track.Info = fmt.Sprintf("%s - %s", m.Artist(), m.Title())
+		}
+		tracks = append(tracks, track)
+	}
+	pl := &Playlist{
+		Title:  NormalizeForTitle(cmusPlaylistPath),
+		Tracks: tracks,
+	}
+	return pl.ToFile(Filename(cmusPlaylistPath, outputPath))
 }
